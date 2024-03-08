0fc1efe7fd94e1e0646e0cf68688212741d99828
amirhossein <76752605+amirbirang@users.noreply.github.com>
2024-03-08 15:48:03 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for( int num = 100; num <= 999;num++)
+            {
+                int help, digit1, digit2, digit3;
+                digit3 = num % 10;
+                help = num / 10;
+                digit2 = help % 10;
+                digit1 = help / 10;
+                digit1 = digit1 * digit1 * digit1;
+                digit2 = digit2 * digit2 * digit2;
+                digit3 = digit3 * digit3 * digit3;
+                if(num==digit3+digit2+digit1)
+                {
+                    Console.WriteLine(num);
+                }
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
